be50ae0c435cb214f11759eda5411621b3b0534a
Mateus Seixas <mateuseixas@icloud.com>
2023-09-19 20:35:36 -0300

    feature: Styles configurated

---
+++ web/src/assets/styles/themes/light.ts
@@ -0,0 +1,11 @@
+export const lightTheme = {
+  title: "light",
+  colors: {
+    primary: "#0485FF",
+    secondary: "#00CC99",
+    red: "#C90808",
+    white: "#FFFFFF",
+    gray: "#EFEDED",
+    disabled: "#E4E4E4",
+  },
+};
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./assets/styles/global.ts";
+import { lightTheme } from "./assets/styles/themes/light.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={lightTheme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
